90a325741ad0892d46c69e2a825a59313bc82edf
johnny0508 <35571401+johnny0508@users.noreply.github.com>
2018-05-12 11:19:22 +0800

    0

--- test.py
+++
@@ -1,7 +0,0 @@
-
-from PIL import Image
-import numpy as np
-
-im_address = r"C:\Users\chouhin\Desktop\105409528\0\0-1.bmp"
-im = Image.open(im_address)
-im.show()
--- week4_closest point.py
+++
@@ -1,5 +0,0 @@
-times=int(input())
-
-for i in range(times):
-    m=input()
-    for m in r
